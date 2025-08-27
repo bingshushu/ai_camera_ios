fde9cc0e86f9bb0d1fe14728876f91b640fa9045
申兵兵 <601367322@qq.com>
2025-08-27 18:40:30 +0800

    Initial Commit

---
+++ AICamera-iOS/AICamera_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICamera_iOSApp.swift
+//  AICamera-iOS
+//
+//  Created by Sen on 2025/8/27.
+//
+
+import SwiftUI
+
+@main
+struct AICamera_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
